3b1cc52b84ac07c3b45bbc2a685cf7c8eb9f79d8
youfree568 <youfree568@gmail.com>
2022-06-26 17:53:09 +0300

    move ship in oposit direction and not away from the screen

--- ship.py
+++ ship.py
@@ -15,9 +15,9 @@ class Ship:
 		self.x = float(self.rect.x)
 
 	def update(self):
-		if self.moving_right:
+		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.x += self.settings.ship_speed
-		elif self.moving_left:
+		if self.moving_left and self.rect.left > 0:
 			self.x -= self.settings.ship_speed		
 		# оновити об'єкт rect з self.x
 		self.rect.x = self.x
